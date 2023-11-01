ae3a97ef662221ae11aa71c3afe57d444afffb80
Elsa <elsa.tamisier@zaclys.net>
2023-11-01 13:13:19 -0400

    country security index

--- resources/views/ressources/in-the-world/pages/country.blade.php
+++ resources/views/ressources/in-the-world/pages/country.blade.php
@@ -6,12 +6,24 @@
 @endsection
 
 
+@php
+    $security_index = 0;
+    foreach ($country->organisations as $organisation) {
+        $security_index += $organisation->security_index;
+    }
+    $security_index /= count($country->organisations);
+    $security_index += 50;
+@endphp
 
 
 @section('content')
 
     <div class="p-4">
         <h2 class="text-center mb-4">{{ $country->name }}</h2>
+
+        <ul>
+            <li><span class="font-weicght-bold">Index de sécurité</span> : {{ $security_index }} %</li>
+        </ul>
 
         <h4 class="text-center mb-3">Organisations</h4>
 
